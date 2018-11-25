3a5070ef0994d1458104ecdeeb470abb6d2d32f4
Florent BARTOLI <flbartol@e3r3p17.42.fr>
2018-11-25 19:50:35 +0100

    memset

--- srcs/ft_memset.c
+++ srcs/ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: flbartol <flbartol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 11:53:26 by flbartol          #+#    #+#             */
-/*   Updated: 2018/11/14 22:34:18 by flbartol         ###   ########.fr       */
+/*   Updated: 2018/11/25 19:50:23 by flbartol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset1(void *b, int c, size_t len)
 {
 	while (len--)
 		*(((unsigned char*)b) + len) = (unsigned char)c;
